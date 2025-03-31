f9703da4e79eb5312b43c1a345629ca80f024546
three-bros <liusiqi707@gmail.com>
2025-03-31 15:40:42 +0800

    Add test proj

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace MyLibrary
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
