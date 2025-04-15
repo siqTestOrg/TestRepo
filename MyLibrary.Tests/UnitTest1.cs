3756d6ada61a9b51140683362167b6b76658680d
three-bros <liusiqi707@gmail.com>
2025-04-15 14:00:34 +0800

    Update UnitTest1.cs

--- MyLibrary.Tests/UnitTest1.cs
+++ MyLibrary.Tests/UnitTest1.cs
@@ -6,7 +6,7 @@ public class UnitTest1
     [Fact]
     public void Test_Subtract()
     {
-        var Calculator = new Calculator(); 
+        var Calculator = new Calculator();
         int result = Calculator.Subtract(2, 3);
         Assert.Equal(-1, result);  
     }
