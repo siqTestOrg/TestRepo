bbaf361aa99559e0032c2f7585e43f706c4a927a
three-bros <liusiqi707@gmail.com>
2025-07-17 11:07:14 +0800

    test end

--- MyLibrary/Class2.cs
+++ MyLibrary/Class2.cs
@@ -8,7 +8,7 @@ namespace MyLibrary
             return a - b; 
         }  
         public int Add2(int a, int b)
-        {   
+        {    
             return a + b;    
         }
         public int Subtract2(int a, int b)
